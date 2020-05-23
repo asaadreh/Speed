e57479b5e4c7167f8cbf882616d93da3919e5566
Agha Saad Rehman <a.saad.reh@gmail.com>
2020-05-23 13:25:52 +0500

    Initial Commit

---
+++ Speed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Speed
+//
+//  Created by Agha Saad Rehman on 23/05/2020.
+//  Copyright © 2020 Agha Saad Rehman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
